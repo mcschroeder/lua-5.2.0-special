fd285436ff5e7d2b784a9f50d2976c9c32d77b65
Michael Schröder <mc.schroeder@gmail.com>
2012-01-23 13:28:58 +0100

    fix despec of 0-result calls

--- src/lvmspec.c
+++ src/lvmspec.c
@@ -127,7 +127,7 @@ void despecialize (Proto *p, int reg, RegInfo *reginfo) {
       case OP_CALL: {
         int a = GETARG_A(*i);
         int nresults = GETARG_C(*i) - 1;
-        if (nresults == LUA_MULTRET) break;
+        if (nresults < 1) break; /* also handles LUA_MULTRET */
         if (a <= reg && reg <= a+nresults) {
           int *exptypes = p->exptypes[pc].ts;
           exptypes[reg-a] = LUA_TNONE;
